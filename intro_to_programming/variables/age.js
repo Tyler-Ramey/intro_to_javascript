300df0805580ecb296611f308b1b720609cc6e5c
Tyler Ramey <tyramey95@gmail.com>
2022-10-13 11:05:27 +0000

    Adding exercises from variables chapter

---
+++ intro_to_programming/variables/age.js
@@ -0,0 +1,10 @@
+const AGE = 27;
+const AGE_10 = AGE + 10;
+const AGE_20 = AGE + 20;
+const AGE_30 = AGE + 30;
+const AGE_40 = AGE + 40;
+
+console.log(`In 10 years, you will be ${AGE} years oldl.`);
+console.log(`In 20 years, you will be ${AGE_10} years oldl.`);
+console.log(`In 30 years, you will be ${AGE_20} years oldl.`);
+console.log(`In 40 years, you will be ${AGE_30} years oldl.`);
