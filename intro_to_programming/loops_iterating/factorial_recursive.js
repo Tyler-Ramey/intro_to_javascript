dcb7a9d417daf1749ceaa27e8b0acee0822795fe
Tyler Ramey <tyramey95@gmail.com>
2022-10-14 19:06:37 +0000

    updated factorial_recursive to new build

--- intro_to_programming/loops_iterating/factorial_recursive.js
+++ intro_to_programming/loops_iterating/factorial_recursive.js
@@ -1,15 +1,16 @@
-function factorial(num) {
-  let result = 1;
-  for (num; num > 0; num -= 1) {
-    result *= num;
+function factorial(number) {
+  if (number === 1) {
+    return 1;
   }
-  
-  return result;
+
+  return number * factorial(number - 1);
 }
 
-function factorialRecursion(num) {
-  
-}
-console.log(factorial(5));
-console.log(factorial(6));
-console.log(factorial(7));
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
